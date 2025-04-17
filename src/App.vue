ea0da31b12fcb73794dfd93e6badef917ad49682
ohhjee <orotunjigbega@gmail.com>
2025-04-17 16:30:35 +0100

    fix error

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-const textColor = ref("");
+const textColor = ref<string[]>();
 const colors = [
   "#1e90ff",
   "#87ceeb",
@@ -19,6 +19,15 @@ const toolKit = [
   "Javascript",
   "Css",
 ];
+const toolKitIcon = [
+  "teenyicons:typescript-outline",
+  "famicons:logo-laravel",
+  "teenyicons:javascript-outline",
+  "iconoir:git-solid",
+  "ri:vuejs-line",
+  "akar-icons:css-fill",
+  "akar-icons:php-fill",
+];
 
 const getRandomColor = () => {
   return colors[Math.floor(Math.random() * colors.length)];
@@ -29,7 +38,7 @@ onMounted(() => {
 });
 </script>
 <template>
-  <div class="tw:max-w-8/12 tw:mx-auto">
+  <div class="tw:max-sm:p-4 tw:min-sm:max-w-8/12 tw:mx-auto">
     <div class="tw:flex tw:items-center tw:justify-between tw:py-3">
       <router-link to="#" class="tw:font-dancing tw:text-white tw:text-4xl"
         >Ohhjee</router-link
@@ -52,33 +61,39 @@ onMounted(() => {
           />
         </span>
       </div>
-      <div class="tw:max-w-xl">
-        <div class="tw:text-6xl tw:font-extrabold tw:text-shadow-lg">
+      <div class="tw:min-ml:max-w-xl tw:max-lg:w-full">
+        <div
+          class="tw:min-lg:text-6xl tw:max-lg:text-2xl tw:font-extrabold tw:text-shadow-lg"
+        >
           I am <span class="tw:tracking-wide"> Oluwagbenga </span>
         </div>
-        <div class="tw:text-6xl textShadow tw:font-extrabold">
+        <div
+          class="tw:min-lg:text-6xl tw:max-lg:text-2xl textShadow tw:font-extrabold"
+        >
           A Web Developer
         </div>
-        <div class="tw:text-lg">
+        <div class="tw:min-sm:text-lg tw:text-xs">
           Full-stack Developer specializing in Vue.js & Laravel.
         </div>
       </div>
       <div class="tw:my-20 tw:flex tw:items-start tw:justify-between tw:gap-10">
-        <div class="tw:gap-4 tw:flex tw:items-center">
+        <div
+          class="tw:gap-4 tw:flex tw:items-center tw:min-lg:flex-row tw:max-lg:flex-col tw:w-full"
+        >
           <div class="tw:rounded-full tw:size-40 tw:overflow-hidden tw:avatar">
             <img
               src="https://loremflickr.com/320/240/dog"
               class="tw:size-full"
             />
           </div>
-          <span class="tw:text-lg tw:max-w-sm">
+          <div class="tw:text-lg tw:max-w-sm tw:max-md:text-center">
             I am passionate about crafting solid and scalable products with
             great user experiences.
-          </span>
+          </div>
         </div>
       </div>
       <section>
-        <div class="tw:text-xl tw:text-justify">
+        <div class="tw:text-xl tw:min-lg:text-justify">
           I craft seamless, user-focused web applications by blending dynamic
           front-ends with robust backends. With expertise in
           <span class="tw:text-lightblue tw:font-bold tw:link">Vue.js</span> for
@@ -104,12 +119,30 @@ onMounted(() => {
         <div
           class="tw:grid tw:min-md:grid-cols-3 tw:min-sm:grid-cols-2 tw:gap-5 tw:font-bold tw:grid-cols-1"
         >
-          <div
+          <!-- <div
             v-for="(tools, index) in toolKit"
             :key="index"
             class="tw:text-xl toolkit"
           >
             {{ tools }}
+          </div> -->
+          <!-- <div
+            v-for="(tools, index) in toolKitIcon"
+            :key="index"
+            class="tw:text-xl toolkit tw:bg-gray-100 tw:shadow-2xl"
+          >
+            <iconify :icon="tools" class="tw:text-white" />
+          </div> -->
+        </div>
+        <div
+          class="tw:grid tw:min-md:grid-cols-7 tw:min-sm:grid-cols-2 tw:gap-5 tw:font-bold tw:grid-cols-2"
+        >
+          <div
+            v-for="(tools, index) in toolKitIcon"
+            :key="index"
+            class="tw:text-xl tw:bg-white/5 tw:w-fit tw:py-4 tw:px-10 tw:rounded-2xl tw:last:col-span-1 tw:hover:shadow-[0px_0px_5px] tw:shadow-gray-100 tw:hover:transition-all"
+          >
+            <iconify :icon="tools" class="tw:text-white/20 tw:size-8" />
           </div>
         </div>
       </section>
