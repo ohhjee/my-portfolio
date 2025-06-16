6eecf7af25388a60f3746abce4cf2a1661c0e5c4
ohhjee <orotunjigbega@gmail.com>
2025-06-16 17:50:09 +0100

    ~wip:update on home page

--- src/views/project/index.vue
+++ src/views/project/index.vue
@@ -1,59 +1,59 @@
 <script lang="ts" setup>
-import type { IsWork } from "@/definition";
+// import type { IsWork } from "@/definition";
 
-const works = [
-  {
-    id: 1,
-    company: "Buysimply",
-    location: "Lagos, Nigeria",
-    year: "2024",
-    title: "Frontend Developer",
-    role: "Contract",
-    description:
-      "I built the complete frontend architecture, styling, and component logic. API connectivity was implemented by a backend developer I collaborated with.",
-    languages: ["Vue", "Vuetify", "Typescript"],
-    icons: ["devicon:vuejs", "devicon:vuetify", "devicon:typescript"],
-    img: "/img/buySimply.png",
-    link: "https://web.buysimply.app/",
-    keyResponsibilities: [
-      {
-        id: 1,
-        // res:
-      },
-    ],
-  },
-  {
-    id: 2,
-    company: "TriftToWin",
-    location: "Lagos, Nigeria",
-    year: "2024",
-    title: "Frontend Developer",
-    role: "Contract",
-    description:
-      "I built the complete frontend architecture, styling, and component logic. I also connected the api.",
-    languages: ["Vue", "Vuetify", "Typescript"],
-    icons: ["devicon:vuejs", "devicon:vuetify", "devicon:typescript"],
-    img: "/img/trift.png",
-    link: "https://play.google.com/store/apps/details?id=com.playthriftowin",
-  },
-  {
-    id: 3,
-    company: "E skool",
-    location: "Lagos, Nigeria",
-    year: "2022",
-    title: "Frontend Developer",
-    role: "Contract",
-    description:
-      "I built the complete frontend architecture, styling, and component logic.",
-    languages: ["React", "Tailwind", "Typescript"],
-    icons: [
-      "vscode-icons:file-type-reactjs",
-      "devicon:tailwindcss",
-      "devicon:typescript",
-    ],
-    img: "/img/eskool.png",
-    link: "https://www.results.eskoolnow.com/",
-  },
-] as IsWork[];
+// const works = [
+//   {
+//     id: 1,
+//     company: "Buysimply",
+//     location: "Lagos, Nigeria",
+//     year: "2024",
+//     title: "Frontend Developer",
+//     role: "Contract",
+//     description:
+//       "I built the complete frontend architecture, styling, and component logic. API connectivity was implemented by a backend developer I collaborated with.",
+//     languages: ["Vue", "Vuetify", "Typescript"],
+//     icons: ["devicon:vuejs", "devicon:vuetify", "devicon:typescript"],
+//     img: "/img/buySimply.png",
+//     link: "https://web.buysimply.app/",
+//     keyResponsibilities: [
+//       {
+//         id: 1,
+//         // res:
+//       },
+//     ],
+//   },
+//   {
+//     id: 2,
+//     company: "TriftToWin",
+//     location: "Lagos, Nigeria",
+//     year: "2024",
+//     title: "Frontend Developer",
+//     role: "Contract",
+//     description:
+//       "I built the complete frontend architecture, styling, and component logic. I also connected the api.",
+//     languages: ["Vue", "Vuetify", "Typescript"],
+//     icons: ["devicon:vuejs", "devicon:vuetify", "devicon:typescript"],
+//     img: "/img/trift.png",
+//     link: "https://play.google.com/store/apps/details?id=com.playthriftowin",
+//   },
+//   {
+//     id: 3,
+//     company: "E skool",
+//     location: "Lagos, Nigeria",
+//     year: "2022",
+//     title: "Frontend Developer",
+//     role: "Contract",
+//     description:
+//       "I built the complete frontend architecture, styling, and component logic.",
+//     languages: ["React", "Tailwind", "Typescript"],
+//     icons: [
+//       "vscode-icons:file-type-reactjs",
+//       "devicon:tailwindcss",
+//       "devicon:typescript",
+//     ],
+//     img: "/img/eskool.png",
+//     link: "https://www.results.eskoolnow.com/",
+//   },
+// ] as IsWork[];
 </script>
 <template></template>
